47484ac5e242752e811014948287362039bfdfe9
Luísa <luisanovara@gmail.com>
2016-03-25 15:14:57 -0300

    Adicoes no script do hipercubo, para incluir hipercubo rodado em 25 de marco de 2016

--- analises/teste_analises.R
+++ analises/teste_analises.R
@@ -9,6 +9,10 @@ input<-matrix(data=c(20,10,1,2,40,40,1,1,0,0,0,0,100,100,10,10),nrow=2)
 input<-data.frame(input)
 input
 
+input<-matrix(data=c(20,10,40,40,1,1,0,0,0,0),nrow=2)
+input<-data.frame(input)
+input
+
 input<-matrix(data=c(20,10,250,500,10000,10000,1,1,0,0,0,0,100,100,10,10),nrow=2)
 input2<-data.frame(input2)
 input2
@@ -23,3 +27,71 @@ dp=0.5
 ciclo=100
 step=10
 
+#### novos testes
+teste_analise8<-simula_externa_input(input=dados1,Jt=100)
+teste_analise9<-simula_externa_input(input=data.frame(c(100,100),c(10000,10000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=1000)
+teste_analise10<-simula_externa_input(input=data.frame(c(100,100),c(10000,10000),c(2,2),c(1,1),c(0.2,0.2)),Jt=1000) #dá muito o problema
+teste_analise11<-simula_externa_input(input=data.frame(c(10,10),c(1000,1000),c(2,2),c(1,1),c(0.2,0.2)),Jt=100)
+teste_analise12<-simula_externa_input(input=data.frame(c(5,5),c(1000,1000),c(2,2),c(1,1),c(0.2,0.2)),Jt=100) #dá mais o problema do que o 11
+teste_analise13<-simula_externa_input(input=data.frame(c(100,100),c(10000,10000),c(5,5),c(1,1),c(0.2,0.2)),Jt=1000) #dá muito o problema 
+
+teste_analise14<-simula_externa_input(input=data.frame(c(5,5),c(500,500),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #não deu problema
+teste_analise15<-simula_externa_input(input=data.frame(c(5,5),c(500,500),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=100) #deu problema às vezes 
+teste_analise16<-simula_externa_input(input=data.frame(c(5,5),c(500,500),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=250) #deu problema sempre
+teste_analise17<-simula_externa_input(input=data.frame(c(5,5),c(750,750),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=250) #deu problema sempre
+teste_analise18<-simula_externa_input(input=data.frame(c(5,5),c(1000,1000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #não deu problema
+teste_analise19<-simula_externa_input(input=data.frame(c(5,5),c(2000,2000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #não deu problema
+teste_analise20<-simula_externa_input(input=data.frame(c(5,5),c(10000,10000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #nao deu problema
+
+##
+teste_analise21<-simula_externa_input(input=data.frame(c(5,5),c(12255,10000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #problema do valor de X/J: resolvido
+teste_analise22<-simula_externa_input(input=data.frame(c(6,5),c(12250,10000),c(0.5,0.5),c(1,1),c(0.2,0.2)),Jt=50) #problema do valor de S: ok
+teste_analise23<-simula_externa_input(input_hipercubo=input,Jt=50) #problema da nomenclatura do input_hipercubo
+teste_analise24<-simula_externa_input(input_hipercubo=round(get.data(uncoupled_hipercubo)),Jt=50) #muito lento, nao roda... o jeito é resolver o problema da nomenclatura na propria simula_externa_input.. o que eh melhor mesmo, ja que eu quero arredondar alguns valores sorteados pelo hipercubo (nao faz sentido 0.5 especies, por exemplo)
+teste_analise25<-simula_externa_input(fun=simula_externa_output,input_hipercubo=input,Jt=50,ciclos=1000,steps=100) #igual a teste_analise14, mas com input embutido. Nao existia problema de input, era soh qual input estava sendo usado..
+teste_analise26<-simula_externa_input(input_hipercubo=input,Jt=50) #deu certo
+
+teste_analise27<-simula_externa_input(input_hipercubo=input) #DEU PAU... porque X eh menor do que J.
+
+####### Testes com valores que irei utilizar antes de resolver as issues
+# Variando a riqueza de 1 a 50
+input=data.frame(c(1,seq(0,50,5)[-1]),rep(5000,11),rep(0.5,11),rep(0,11),rep(0,11))
+teste_analise28<-simula_externa_input(input_hipercubo=input,Jt=50) #quando S=1, nao houve variancia entre individuos, o que eh estranho..
+# Variando o X de 5000 a 500000
+input=data.frame(rep(5,11),seq(5000,500000,length=11),rep(0.5,11),rep(0,11),rep(0,11))
+teste_analise29<-simula_externa_input(input_hipercubo=input,Jt=50)
+# Variando o dp de 0 a 1
+input=data.frame(rep(5,11),rep(5000,11),seq(0,1,length=11),rep(0,11),rep(0,11))
+teste_analise30<-simula_externa_input(input_hipercubo=input,Jt=50) #quando dp=0, 0.1 e 0.3, nao houve variancia entre individuos. Mas 0.2 houve
+# Variando o dist.int de 0 a 1 (no ciclo 1)
+input=data.frame(rep(5,11),rep(5000,11),rep(0.5,11),c(0,rep(1,10)),seq(0,1,length=11))
+teste_analise31<-simula_externa_input(input_hipercubo=input,Jt=50)
+
+input=data.frame(rep(100,10),rep(10000,10),rep(0.5,10),rep(1,10),rep(0.5,10))
+teste_analise32<-simula_externa_input(input_hipercubo=input,Jt=1000)
+input=data.frame(rep(1000,10),rep(100000,10),rep(0.5,10),rep(1,10),rep(0.5,10))
+teste_analise33<-simula_externa_input(input_hipercubo=input,Jt=10000)
+
+################ Testes apos reuniao com o Ale ################
+## Correcao do problema do ciclo
+input=data.frame(c(5,5),c(10,10),c(2,2),c(3,900),c(0.2,0.5))
+teste_analise34<-simula_externa_input(input_hipercubo=input,Jm=5000)
+input=data.frame(c(5,5),c(10,10),c(2,2),c(1,1),c(0.2,0))
+teste_analise34<-simula_externa_input(input_hipercubo=input,Jm=50)
+
+input=data.frame(c(7,11),c(10,10),c(2,2),c(1000,1000),c(0.5,0.5))
+
+## testes 05/03/2016
+t1<-simula.neutra.step(S=10,j=10,xi0=10,dp=1,dist.pos=100,dist.int=0.5,ciclo=1000,step=100)
+t2<-simula.neutra.step(5,1000,10,2,1,0.2,1000,100)
+
+t3<-simula_input(input,100,1000,100)
+t3o<-simula_output(t3)
+
+######## Testes "finais"
+input=data.frame(c(500,5),c(1000,10),c(5,2),c(50,1),c(0.5,0))
+t4<-simula_input(input,5000,1000,100)
+t4o<-simula_output(t4)
+input=data.frame(c(5,5),c(100,100),c(5,0.1),c(100,100),c(0.5,0.5))
+t5<-simula_input(input,5000,1000,100)
+t5o<-simula_output(t5)
--- hipercubo/hipercubo_simula.neutra.step.R
+++ hipercubo/hipercubo_simula.neutra.step.R
@@ -1,142 +1,35 @@
-###################### MINHA FUNÇÃO: #####################
-################### SIMULA.NEUTRA.STEP ###################
-simula.neutra.step=function(S= 100, j=10, X=1000, dp=0.1, ciclo=1e6, step=100)
-{
-  t0=proc.time()[[3]]
-  ## Tamanho da comunidade
-  J <- S*j
-  ##Verifica se X e multiplo de J
-  if(abs(X/J - round(X/J)) > .Machine$double.eps^0.5) ## aqui verifica se o X/J e inteiro ou quase!
-  {
-    stop("\n\tO potencial reprodutivo (X) precisa ser multiplo do tamanho da comunidade (J). Tente novamente!\n\n")
-  } 
-  ##Matrizes para guardar os resultados
-  ## matriz da especie de cada individuo por ciclo
-  ind.mat=matrix(nrow=J,ncol=1+ciclo/step) 
-  ##Matriz de propagulos produzidos por individuo em cada ciclo
-  prop.mat=matrix(nrow=J,ncol=1+ciclo/step)
-  ## Matriz de probabilidade de morte de cada individuo, por ciclo
-  dead.mat=matrix(nrow=J,ncol=1+ciclo/step)
-  ##Vetor com numero de mortos por ciclo
-  n.dead <- c()
-  n.dead[1] <- 0
-  ##CONDICOES INICIAIS##
-  ##Deduzidas de acordo com o modelo de Hubbell:
-  ## Todas as especies comecam com o mesmo numero de individuos (j=J/S)
-  ind.mat[,1] <- rep(1:S,each=j)
-  cod.sp <- ind.mat[,1]
-  ##A probabilidade inicial de morte, tomada de uma geometrica,
-  ##dado que o numero de mortes esperado por ciclo eh a mesma para todos (p=1/J)
-  dead.mat[,1] <- 1/J
-  p.death <- dead.mat[,1]
-  ##O esforco reprodutivo inicial e deduzido da esperanca de tempo de vida da geometrica E[t]=J
-  ## o que portanto resulta em X/J propagulos produzidos a cada ciclo, por todos
-  prop.mat[,1] <- X/J
-  n.propag <- prop.mat[,1]
-  ##Aqui comecam as simulacoes
-  for(i in 2:(1+ciclo/step))
-  {
-    n.mortes <- 0
-    for(a in 1:step) 
-    {
-      ## Sorteio dos que morrerao
-      morte=rbinom(J, 1, prob=p.death)
-      ##Total de mortos, que e armazenado em n.dead
-      D=sum(morte)
-      n.mortes <- n.mortes+D
-      ## Se ha mortos comeca aqui a rotina de substituicao dos valores
-      if(D>0) 
-      {
-        ##vetor de propagulos: cada propagulo tem o codigo numerico do individuo
-        seed.bank <- rep(1:J,n.propag) 
-        ##Indices dos individuos que morreram
-        nascer= which(morte==1)
-        ##Sorteio dos propagulos que irao repor os mortos
-        mami=sample(seed.bank,D)
-        ##Um vetor para armazenar o fenotipo do pai
-        papi <- c()
-        ##Um loop para sortear o pai entre os individuos da especie de cada
-        ## propagulo-mae sorteado
-        for(w in 1:D)
-        {
-          papi[w] <- sample(n.propag[ cod.sp==cod.sp[mami[w]] ],1)
-        }
-        ##O valor esperado de propagulos dos filhotes eh
-        ## a media do numero medio de propagulos produzidos pelo pai e pela mae
-        medias.prop=(n.propag[mami]+papi)/2
-        ##Codigos das especies dos mortos sao substituidos pelos nascidos
-        ##dos propagulos sorteados
-        cod.sp[nascer]<-cod.sp[mami]
-        ## Numero medio de propagulos dos novos individuos nascidos e sorteada
-        ## de uma normal discretizada e truncada entre 1 e J,
-        ## com o desvio padrao estabelecido
-        n.propag[nascer] <- sapply(medias.prop,rnormt,dp=dp,min=1,max=X)
-        ##A matriz de probabilidades de morrer eh atualizada para os novos individuos
-        p.death[nascer] <- n.propag[nascer]/X
-      }
-    }
-    ## A cada step ciclos os resultados sao gravados
-    ind.mat[,i] <- cod.sp
-    dead.mat[,i] <- p.death
-    prop.mat[,i] <- n.propag
-    n.dead[i] <- n.mortes
-    ##cat(format(Sys.time(), "%d%b%Y_%H:%M"), "\t ciclo = ", i, "\n") # para avisar a cada ciclo! desligar se estiver usando Rcloud
-  }
-  tempo <- seq(0,ciclo,by=step)
-  colnames(ind.mat) <- tempo
-  colnames(dead.mat) <- tempo
-  colnames(prop.mat) <- tempo
-  names(n.dead) <- tempo
-  resulta=list(tempo=tempo,sp.list=ind.mat,sementes=prop.mat,prob.morte=dead.mat,n.mortes=n.dead)
-  t1=proc.time()[[3]]
-  cat("\n\t tempo de processamento: ", round((t1-t0)/60,2),"\n") 
-  ## incluindo atributos no arquivo resulta
-  attributes(resulta)$start=list(especies=S, individuos=j, nprop=X, sd=dp, ciclos=ciclo, passos=step)
-  return(resulta)
-}
-################### FUNÇÃO ACESSÓRIA À ###################
-################### SIMULA.NEUTRA.STEP ###################
-rnormt <- function(mean,n=1,dp,min,max)
-{
-  vals <- (min-0.5):(max+0.5)
-  p <- pnorm(vals,mean=mean,sd=dp)
-  p2 <- diff(p)
-  sample(min:max,n,prob=p2,replace=T)
-}
-############### SIMULA.NEUTRA.STEP.EXTERNA ############### 
-simula.neutra.step.externa=function(S, j, X, dp){
-  ciclo <- 100
-  step <- 100
-  if(abs(X/(S*j)) - round(X/(S*j)) > .Machine$double.eps^0.5)
-  {stop}
-  else{
-  x <- simula.neutra.step(S, j, X, dp, ciclo, step)
-  return(as.matrix(x[3]$sementes))}
-}
 ##########################################################
 #################### HIPERCUBO LATINO ####################
 ##########################################################
 require(pse)
+
+##########################################################
+################# SIMULACOES 24/01/2016 ##################
+##########################################################
+
 ################# LISTANDO OS PARÂMETROS #################
 ##################### DA MINHA FUNÇÃO ####################
-factors <- c("S", "j", "X", "dp")
+factors <- c("S","X","dp","dist.pos","dist.int")
 ############### DEFININDO AS DISTRIBUIÇÕES ###############
 ############ DE PROBABILIDADE DOS PARÂMETROS #############
-q <- c("qunif", "qunif", "qunif", "qlnorm")
+q <- c("qunif", "qunif", "qunif","qunif","qunif")
 ################ DEFININDO OS PARÂMETROS #################
 #################### DAS DISTRIBUIÇÕES ###################
-q.arg <- list( list(min=2,max=100), list(min=10,max=1000),
-               list(min=1, max=1000), list(meanlog=0.5,sdlog=1))
-####################### "WRAPPER" ########################
-modelRun <- function (dados) {
-  mapply(simula.neutra.step.externa, dados[,1], dados[,2], dados[,3], dados[,4]) }
+q.arg <- list( list(min=1,max=50), list(min=5000,max=500000), list(min=0, max=1),
+               list(min=1,max=1000), #PROVISORIO...
+               list(min=0,max=1))
+#######################################
+#modelRun <- function (dados) {
+  #mapply(simula.neutra.step.externa, dados[,1], dados[,2], dados[,3], dados[,4]) }
 ################## RODANDO O HIPERCUBO ###################
 #res.name <- c("propagulos/ciclo")
 #hipersuperincrivelcubolatino <- LHS(modelRun, factors, N=10, q, q.arg, res.name, nboot=50)
 uncoupled_hipercubo <- LHS(NULL, factors, N=1000, q, q.arg, nboot=50)
 ######### ACESSANDO OS VALORES USADOS COMO INPUT #########
 #get.data(hipersuperincrivelcubolatino)
-get.data(uncoupled_hipercubo)
+dados<-get.data(uncoupled_hipercubo)
+dados_24jan2016<-cbind(round(dados[,1]),round(dados[,2]),dados[,3],round(dados[,4]),dados[,5]) #arredondando valores de parametros que precisam ser inteiros
+dados_24jan2016<-as.data.frame(dados_24jan2016)
 ################ ACESSANDO OS RESULTADOS #################hip
 #get.results(hipersuperincrivelcubolatino)
 ################# ANÁLISE DE INCERTEZA ###################
@@ -147,4 +40,27 @@ get.data(uncoupled_hipercubo)
 ############### SBMA: A AMOSTRA ESTÁ OK? #################
 #targetLHS <- target.sbma (target=0.7, modelRun, factors, q, q.arg, res.names, FUN=min)
 
+##########################################################
+################# SIMULACOES 25/03/2016 ##################
+##########################################################
+
+################# LISTANDO OS PARÂMETROS #################
+##################### DA MINHA FUNÇÃO ####################
+factors <- c("S","xi0","dp","dist.pos","dist.int")
+############### DEFININDO AS DISTRIBUIÇÕES ###############
+############ DE PROBABILIDADE DOS PARÂMETROS #############
+q <- c("qunif","qunif","qunif","qunif","qunif")
+################ DEFININDO OS PARÂMETROS #################
+#################### DAS DISTRIBUIÇÕES ###################
+q.arg <- list(list(min=5,max=500),
+              list(min=1,max=20000),
+              list(min=0, max=5),
+              list(min=1,max=100000),
+              list(min=0,max=1))
+################## RODANDO O HIPERCUBO ###################
+uncoupled_hipercubo_25mar16 <- LHS(NULL, factors, N=10000, q, q.arg, nboot=50)
+######### ACESSANDO OS VALORES DE INPUT #########
+dados_25mar16<-get.data(uncoupled_hipercubo)
+dados2_25mar16<-cbind(round(dados[,1]),dados[,2],dados[,3],round(dados[,4]),dados[,5]) #arredondando valores de parametros que precisam ser inteiros: S (numero de especies) e dist.pos (intervalo entre eventos de disturbio)
+dados3_25mar16<-as.data.frame(dados2)
 
